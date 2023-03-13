13d21fdc65939053d9eb3161c460bbc60b9cd796
Anil Dwarakanath <anildwa@microsoft.com>
2023-03-13 10:53:19 -0700

    update

---
+++ check-blob-storage-size.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the name of the storage account and container
+storage_account_name="anildwaadlsv2"
+container_name="folder2"
+
+# Get the storage account key
+storage_account_key=$(az storage account keys list --account-name $storage_account_name --query "[0].value" --output tsv)
+
+# Get the size of the container
+container_size=$(az storage blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $container_name --query "[].properties.contentLength" --output tsv | awk '{ sum += $1 } END { print sum }')
+
+echo "Container size: $container_size bytes"
+
+# Get the size of each folder and file
+blobs=$(az storage blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $container_name --query "[].{name: name, size: properties.contentLength}" --output tsv)
+
+while read -r blob; do
+    blob_name=$(echo $blob | awk '{print $1}')
+    blob_size=$(echo $blob | awk '{print $2}')
+    echo "Blob: $blob_name, Size: $blob_size bytes"
+done <<< "$blobs"
